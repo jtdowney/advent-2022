c9a28b557c3cb33510d2dd957d8badceef8b2850
John Downey <jdowney@gmail.com>
2022-12-03 14:03:38 -0600

    Add day 3 of 2022

---
+++ src/day3.rs
@@ -0,0 +1,53 @@
+use std::collections::HashSet;
+
+use aoc_runner_derive::{aoc, aoc_generator};
+
+fn priority(c: char) -> u32 {
+    if c.is_uppercase() {
+        u32::from(c) - 64 + 26
+    } else {
+        u32::from(c) - 96
+    }
+}
+
+#[aoc_generator(day3)]
+fn generator(input: &str) -> Vec<String> {
+    input.lines().map(str::to_string).collect()
+}
+
+#[aoc(day3, part1)]
+fn part1(input: &[String]) -> usize {
+    input
+        .iter()
+        .map(|sack| {
+            let length = sack.len();
+            let chars = sack.chars().collect::<Vec<_>>();
+            let (left, right) = chars.split_at(length / 2);
+
+            let left = left.iter().collect::<HashSet<_>>();
+            let right = right.iter().collect::<HashSet<_>>();
+            (&left & &right)
+                .into_iter()
+                .map(|&c| priority(c) as usize)
+                .sum::<usize>()
+        })
+        .sum::<usize>()
+}
+
+#[aoc(day3, part2)]
+fn part2(input: &[String]) -> usize {
+    input
+        .chunks(3)
+        .map(|sacks| {
+            let badges = sacks
+                .iter()
+                .map(|sack| sack.chars().collect::<HashSet<_>>())
+                .reduce(|acc, item| &acc & &item);
+
+            badges
+                .iter()
+                .map(|chars| chars.iter().map(|&c| priority(c) as usize).sum::<usize>())
+                .sum::<usize>()
+        })
+        .sum::<usize>()
+}
